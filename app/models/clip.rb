a8caa09d715e9a7bda61d5aaaf6afa38a271b566
olinelson <olivernelson@icloud.com>
2019-04-25 11:36:29 -0400

    screenshots generated on upload but still need refresh

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -161,7 +161,7 @@ def generate_video_thumbnail
   screenshot = movie.screenshot("tmp/screenshots/#{self.id}_screenshot.jpg")
   self.image.attach(io: File.open("./tmp/screenshots/#{self.id}_screenshot.jpg"), filename: "#{self.id}_screenshot.jpg")
   self.gcloud_image_link = "https://storage.googleapis.com/bucket-of-doom/#{self.image.key}"
-
+  self.save
 end
 
 
