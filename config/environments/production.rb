c3eae7307587bdd19031bcdc008d0bb250273af8
Oli Nelson <olivernelson@icloud.com>
2020-01-09 12:14:41 -0500

    changed to gmail notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,24 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.default :charset => "utf-8"
 
+# config.action_mailer.smtp_settings = {
+#     user_name:      ENV['GMAIL_USERNAME'],
+#     password:       ENV['GMAIL_PASSWORD'],
+#     from:           ENV['GMAIL_USERNAME'],
+#     address:        'smtp.office365.com',
+#     port:           '587',
+#     authentication: 'login',
+# }
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
 config.action_mailer.smtp_settings = {
-    user_name:      ENV['OFFICE_USERNAME'],
-    password:       ENV['OFFICE_PASSWORD'],
-    from:           ENV['OFFICE_USERNAME'],
-    address:        'smtp.office365.com',
-    port:           '587',
-    authentication: 'login',
-}
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
